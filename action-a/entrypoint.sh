1b18cd326e4ea5d6133cc5f18dd465e2eac6b828
natkozak <natkozak0@gmail.com>
2021-07-15 15:43:31 -0700

    Refactor to use javascript instead of dockerfile

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
+# "echo Hello world my name is $INPUT_MY_NAME" would also work because we're already in a shell script -- we don't need another shell to run a shell script
